914c90f67b8eefbdda98492a4fe7f5cd6dcf72cf
max-student <max-student@maxtrain.com>
2020-01-29 16:01:53 -0500

    update

--- class-instructor.sql
+++ class-instructor.sql
@@ -1,5 +1,11 @@
-select concat(firstname,' ',lastname) as 'Name',YearsExperience,IsTenured,Subject,Section
+--SELECT * FROM Instructor JOIN Class ON Instructor.ID = Class.InstructorId;
+
+
+select CLASS.ID AS 'Course ID #',concat(Subject, ' ',Section) as 'Course name',concat(firstname,' ',lastname) as 'Teacher Name',YearsExperience as 'Exp.'
 	from Instructor
-	join Class 
-		on instructor.id = class.InstructorId 
-	order by Instructor.Lastname
+	join Class
+		on INSTRUCTOR.id = CLASS.InstructorId 
+		where IsTenured = 1
+	order by Lastname;
+
+--select * from Instructor
